4089eba23d267548b2827dd59fcb4517226cc5f8
airtosupply <airtosupply@126.com>
2023-03-30 11:38:48 +0800

    [refactor] Optimize InfluxDB Client

--- src/main/scala/org/apache/spark/sql/influxdb/util/FluxClient.scala
+++ src/main/scala/org/apache/spark/sql/influxdb/util/FluxClient.scala
@@ -8,22 +8,37 @@ import com.influxdb.query.FluxRecord
 import com.influxdb.query.dsl.Flux
 import com.influxdb.query.dsl.functions.restriction.Restrictions
 import org.apache.spark.internal.Logging
+import org.apache.spark.sql.influxdb.util.FluxClient._
 
 import java.time.Instant
 import java.util.UUID
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
-object Fluxs extends Logging {
+class FluxClient(options: InfluxDBClientOptions) extends Logging {
 
-  private val RANGE_START = -1L
+  private implicit var _system: ActorSystem = _
+
+  private var _client: InfluxDBClientScala = _
+
+  onStart
+
+  private def onStart = {
+    _system = _createActorSystem
+    _client = _createClient(options)
+  }
+
+  def onStop = {
+    _closeClient(_client)
+    _terminateActorSystem(_system)
+  }
 
   private def _createClient(options: InfluxDBClientOptions) =
     InfluxDBClientScalaFactory.create(options)
 
   private def _closeClient(client: InfluxDBClientScala) = client.close()
 
-  private def _createActorSystem = ActorSystem(UUID.randomUUID().toString)
+  private def _createActorSystem = ActorSystem(ACTOR_SYSTEM_NAME)
 
   private def _terminateActorSystem(actorSystem: ActorSystem) = actorSystem.terminate()
 
@@ -43,113 +58,102 @@ object Fluxs extends Logging {
     }
   }
 
-  def minimumTime(options: InfluxDBClientOptions)
-                 (bucket: String, measurement: String): Option[Instant] = {
-    implicit val system = _createActorSystem
-    val client = _createClient(options)
+  def minimumTime(bucket: String, measurement: String): Option[Instant] = {
+    StopWatch.start
     var _time: Option[Instant] = None
     try {
       val query = Flux.from(bucket)
         .range(RANGE_START)
         .filter(Restrictions.measurement().equal(measurement))
         .first()
-      val results = client.getQueryScalaApi().query(query.toString)
+      val results = _client.getQueryScalaApi().query(query.toString)
       val sink = results.runWith(Sink.foreach[FluxRecord](record => {
         _time = Some(record.getTime)
       }))
       Await.result(sink, Duration.Inf)
+      StopWatch.stop
+      logInfo(s"Get minimum _time cost time: ${StopWatch.cost}ms")
       _time
     } catch {
       case e: Exception =>
         logError("Get minimum _time failed!", e)
-        _closeClient(client)
-        _terminateActorSystem(system)
         _time
-    } finally {
-      _closeClient(client)
-      _terminateActorSystem(system)
     }
   }
 
-  def maximumTime(options: InfluxDBClientOptions)
-                 (bucket: String, measurement: String): Option[Instant] = {
-    implicit val system = _createActorSystem
-    val client = _createClient(options)
+  def maximumTime(bucket: String, measurement: String): Option[Instant] = {
+    StopWatch.start
     var _time: Option[Instant] = None
     try {
       val query = Flux.from(bucket)
         .range(RANGE_START)
         .filter(Restrictions.measurement().equal(measurement))
         .last()
-      val results = client.getQueryScalaApi().query(query.toString)
+      val results = _client.getQueryScalaApi().query(query.toString)
       val sink = results.runWith(Sink.foreach[FluxRecord](record => {
         _time = Some(record.getTime)
       }))
       Await.result(sink, Duration.Inf)
+      StopWatch.stop
+      logInfo(s"Get maximum _time cost time: ${StopWatch.cost}ms")
       _time
     } catch {
       case e: Exception =>
         logError("Get maximum _time failed!", e)
-        _closeClient(client)
-        _terminateActorSystem(system)
         _time
-    } finally {
-      _closeClient(client)
-      _terminateActorSystem(system)
     }
   }
 
-  def count(options: InfluxDBClientOptions)
-           (bucket: String, measurement: String, start: Instant, stop: Instant): Option[Long] = {
-    implicit val system = _createActorSystem
-    val client = _createClient(options)
+  def count(bucket: String, measurement: String, start: Instant, stop: Instant): Option[Long] = {
+    StopWatch.start
     var _count: Option[Long] = None
     try {
       val query = Flux.from(bucket)
         .range(start, stop)
         .filter(Restrictions.measurement().equal(measurement))
         .count
-      val results = client.getQueryScalaApi().query(query.toString)
+      val results = _client.getQueryScalaApi().query(query.toString)
       val sink = results.runWith(Sink.foreach[FluxRecord](record => {
         // [TIPS] Get real row size, or not field row
         _count = Some(record.getValue.toString.toLong)
       }))
       Await.result(sink, Duration.Inf)
+      StopWatch.stop
+      logInfo(s"Get count between time range cost time: ${StopWatch.cost}ms")
       _count
     } catch {
       case e: Exception =>
         logError("Get count between time range failed!", e)
-        _closeClient(client)
-        _terminateActorSystem(system)
         _count
-    } finally {
-      _closeClient(client)
-      _terminateActorSystem(system)
     }
   }
 
-  def queryByTimeRange(options: InfluxDBClientOptions)
-                      (bucket: String, measurement: String, start: Instant, stop: Instant) = {
-    implicit val system = _createActorSystem
-    val client = _createClient(options)
+  def queryByTimeRange(bucket: String, measurement: String, start: Instant, stop: Instant) = {
+    StopWatch.start
     val elements = scala.collection.mutable.ArrayBuffer[FluxRecord]()
     try {
       val query = Flux.from(bucket)
         .range(start, stop)
         .filter(Restrictions.measurement().equal(measurement))
-      val results = client.getQueryScalaApi().query(query.toString)
+      val results = _client.getQueryScalaApi().query(query.toString)
       val sink = results.runWith(Sink.foreach[FluxRecord](elements.append(_)))
       Await.result(sink, Duration.Inf)
+      StopWatch.stop
+      logInfo(s"Query between time range cost time: ${StopWatch.cost}ms")
       elements.toIterator
     } catch {
       case e: Exception =>
         logError("Query between time range failed!", e)
-        _closeClient(client)
-        _terminateActorSystem(system)
         elements.toIterator
-    } finally {
-      _closeClient(client)
-      _terminateActorSystem(system)
     }
   }
+}
+
+object FluxClient {
+
+  private val RANGE_START = -1L
+
+  private val ACTOR_SYSTEM_NAME = "influxdb-client-actor-system"
+
+  def apply(options: InfluxDBClientOptions) = new FluxClient(options)
 }
---
+++ src/main/scala/org/apache/spark/sql/influxdb/util/StopWatch.scala
@@ -0,0 +1,13 @@
+package org.apache.spark.sql.influxdb.util
+object StopWatch {
+
+  private var _start: Long = _
+
+  private var _stop: Long = _
+
+  def start = _start = System.currentTimeMillis
+
+  def stop = _stop = System.currentTimeMillis
+
+  def cost = _stop - _start
+}
